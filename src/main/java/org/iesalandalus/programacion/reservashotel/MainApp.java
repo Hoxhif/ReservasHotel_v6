25ee84935be069539df4641749f05e5608da66b2
José A. Guirado González <trizador@gmail.com>
2024-04-06 01:20:42 +0200

    Se ha realizado el MainApp

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,6 +2,7 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
+import org.iesalandalus.programacion.reservashotel.modelo.FactorialFuenteDatos;
 import org.iesalandalus.programacion.reservashotel.modelo.Modelo;
 import org.iesalandalus.programacion.reservashotel.vista.Vista;
 
@@ -11,12 +12,19 @@ public class MainApp {
     public static void main(String[] args) {
         //Inicio del programa.
         try {
-            Modelo modelo = new Modelo();
+            Modelo modelo = new Modelo(procesarArgumentosFuenteDatos(args));
             Vista vista = new Vista();
             Controlador controlador = new Controlador(modelo, vista);
             controlador.comenzar();
         }catch (NullPointerException | IllegalArgumentException e){
             System.out.println("-"+e.getMessage());
         }
+    }
+
+    private static FactorialFuenteDatos procesarArgumentosFuenteDatos(String[] argumentos){
+        for (String cadena: argumentos){
+            if (cadena.equals("-fdmongodb")) return FactorialFuenteDatos.MONGODB;
+            if (cadena.equals("-fdmemoria")) return FactorialFuenteDatos.MEMORIA;
+        }return FactorialFuenteDatos.MEMORIA;
     }
 }
