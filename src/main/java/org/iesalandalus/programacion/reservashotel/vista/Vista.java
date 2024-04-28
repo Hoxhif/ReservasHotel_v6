99b45c35d8ae8dfadd26bc501df6d0bd7dd87146
José A. Guirado González <trizador@gmail.com>
2024-04-28 22:21:49 +0200

    Se ha creado la clase abstracta de Vista con sus metodos y atributos correspondientes.

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Vista.java
@@ -0,0 +1,23 @@
+package org.iesalandalus.programacion.reservashotel.vista;
+
+import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
+
+import java.awt.image.ConvolveOp;
+
+public abstract class Vista {
+    private Controlador controlador;
+
+    public void setControlador(Controlador controlador){
+        this.controlador=controlador;
+    }
+
+    public Controlador getControlador(){
+        return controlador;
+    }
+
+    public abstract void comenzar();
+
+    public abstract void terminar();
+
+
+}
